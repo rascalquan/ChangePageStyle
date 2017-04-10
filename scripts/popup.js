8c52d361a275b9ddb1d384b594f94eeb6a0fd7b6
rascalquan <rascalquan@outlook.com>
2017-04-10 19:29:27 +0800

    save data done

--- scripts/popup.js
+++ scripts/popup.js
@@ -18,10 +18,11 @@ jQuery(function($) {
             },
             {
                 command: [
-                    { name: "destroy", text: "删除" }
+                    { name: "edit", text: "Edit" },
+                    { name: "destroy", text: "Delete" }
                 ],
                 title: "操作",
-                width: "120px"
+                width: "200px"
             }
         ],
         dataSource: {
@@ -32,8 +33,7 @@ jQuery(function($) {
                 create: gridCreate
             },
             // data: [{ dataId: "1", selectorField: "#maindiv", attrKey: "border", attrValue: "1px", activeRange: 0, isAwaysActive: false },
-            //     { dataId: "2", selectorField: "#maindiv", attrKey: "border", attrValue: "1px", activeRange: 0, isAwaysActive: true },
-            //     { dataId: "3", selectorField: "#maindiv", attrKey: "border", attrValue: "1px", activeRange: 0, isAwaysActive: false }
+            //     { dataId: "2", selectorField: "#maindiv", attrKey: "border", attrValue: "1px", activeRange: 0, isAwaysActive: true }
             // ],
             schema: {
                 model: {
@@ -68,14 +68,113 @@ jQuery(function($) {
                 }
             }
         },
-        toolbar: [{ name: "create", text: "新增" }],
-        editable: true,
+        toolbar: [{ name: "create", text: "Add" }],
+        editable: "inline",
         sortable: true,
         //height: "300px"
-        save: gridSave,
+        //save: gridSave,
         //edit: gridUpdate,
-        remove: gridDestroy
+        //remove: gridDestroy
     });
+
+    function attrKeyDropDownEditor(container, options) {
+        $('<input required data-text-field="attrKey" data-value-field="attrKey" data-bind="value:' + options.field + '"/>')
+            .appendTo(container)
+            .kendoDropDownList({
+                autoBind: false,
+                dataSource: {
+                    type: "odata",
+                    transport: {
+                        read: ""
+                    }
+                }
+            });
+    }
+
+    //grid读取数据
+    function gridRead(options) {
+        console.log("begin read");
+        getCurrentTabUrl(function(url) {
+            var attrName = "StyleData_" + url;
+            if (typeof localStorage[attrName] !== "undefined") {
+                var datas = JSON.parse(localStorage[attrName]);
+                options.success(datas);
+            }
+        })
+    }
+    //grid添加数据
+    function gridCreate(options) {
+        var data = getGridData(options);
+        options.success(data);
+        getCurrentTabUrl(function(url) {
+            var attrName = "StyleData_" + url;
+            localStorage[attrName] = JSON.stringify(getAllGridData()); //保存到本地
+        })
+    }
+    //grid更新数据
+    function gridUpdate(options) {
+        console.log("begin Update");
+        var data = getGridData(options);
+        options.success(data);
+        getCurrentTabUrl(function(url) {
+            var attrName = "StyleData_" + url;
+            localStorage[attrName] = JSON.stringify(getAllGridData()); //保存到本地
+        })
+    }
+    //grid删除数据
+    function gridDestroy(e) {
+        getCurrentTabUrl(function(url) {
+            var attrName = "StyleData_" + url;
+            var allData = getAllGridData();
+            if (typeof allData !== "undefined") {
+                localStorage[attrName] = JSON.stringify(allData);
+            } else {
+                delete localStorage[attrName];
+            }
+        })
+    }
+    //获取grid当前行数据
+    function getGridData(options) {
+        var thisData = {
+            dataId: options.data.dataId == "" ? generateUUID() : options.data.dataId,
+            selectorField: options.data.selectorField,
+            attrKey: options.data.attrKey,
+            attrValue: options.data.attrValue,
+            activeRange: options.data.activeRange,
+            isAwaysActive: Boolean(options.data.isAwaysActive)
+        }
+        return thisData;
+    }
+    //获取grid全部数据
+    function getAllGridData() {
+        var allData = $("#mainGrid").data("kendoGrid").dataSource.data();
+        return allData;
+    }
+    //获取url
+    function getCurrentTabUrl(callback) {
+        var queryInfo = {
+            active: true,
+            currentWindow: true
+        };
+        chrome.tabs.query(queryInfo, function(tabs) {
+            var tab = tabs[0];
+            var url = tab.url;
+            callback(url);
+        });
+    }
+
+    //生成UUID
+    function generateUUID() {
+        var d = new Date().getTime();
+        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+            var r = (d + Math.random() * 16) % 16 | 0;
+            d = Math.floor(d / 16);
+            return (c == 'x' ? r : (r & 0x7 | 0x8)).toString(16);
+        });
+        return uuid;
+    };
+
+    //MVVM  暂时不用
     // var viewModel = kendo.observable({
     //     onsave: function(e) {
     //         console.log("begin save.");
@@ -115,97 +214,4 @@ jQuery(function($) {
     //     })
     // })
     // kendo.bind($("#mainGrid"), viewModel);
-
-    function attrKeyDropDownEditor(container, options) {
-        $('<input required data-text-field="attrKey" data-value-field="attrKey" data-bind="value:' + options.field + '"/>')
-            .appendTo(container)
-            .kendoDropDownList({
-                autoBind: false,
-                dataSource: {
-                    type: "odata",
-                    transport: {
-                        read: ""
-                    }
-                }
-            });
-    }
-
-    function gridSave(e) {
-        var data = {
-            dataId: e.model.dataId == "" ? generateUUID() : e.model.dataId,
-            selectorField: e.model.selectorField,
-            attrKey: e.model.attrKey,
-            attrValue: e.model.attrValue,
-            activeRange: e.model.activeRange,
-            isAwaysActive: Boolean(e.model.isAwaysActive)
-        }
-        getCurrentTabUrl(function(url) {
-            if (typeof localStorage["StyleData"] !== "undefined" &&
-                typeof localStorage["StyleData"][url] !== "undefined") {
-                localStorage["StyleData"][url][data.dataId] = data; //保存到本地
-
-            } else if (typeof localStorage["StyleData"] !== "undefined") {
-                localStorage["StyleData"][url] = { a: { a: "a" } };
-                localStorage["StyleData"][url][data.dataId] = data;
-                delete localStorage["StyleData"][url]["a"];
-            } else {
-                localStorage["StyleData"] = { a: { a: "a" } };
-                localStorage["StyleData"][url] = { a: { a: "a" } };
-                localStorage["StyleData"][url][data.dataId] = data;
-                delete localStorage["StyleData"]["a"]["a"];
-                delete localStorage["StyleData"][url]["a"];
-            }
-        })
-    }
-
-    function gridUpdate(e) {
-        console.log("begin Update");
-    }
-
-    function gridDestroy(e) {
-        getCurrentTabUrl(function(url) {
-            delete localStorage["StyleData"][url][e.model.dataId];
-        })
-    }
-
-    function gridCreate(e) {
-        console.log("begin Create");
-    }
-
-    function gridRead(options) {
-        debugger;
-        console.log("begin read");
-        getCurrentTabUrl(function(url) {
-            if (typeof localStorage["StyleData"] !== "undefined" &&
-                typeof localStorage["StyleData"][url] !== "undefined") {
-                var datas = localStorage["StyleData"][url];
-                options.success(datas);
-            }
-        })
-    }
-
-    function getCurrentTabUrl(callback) {
-        var queryInfo = {
-            active: true,
-            currentWindow: true
-        };
-        chrome.tabs.query(queryInfo, function(tabs) {
-            var tab = tabs[0];
-
-            var url = tab.url;
-
-            //console.assert(typeof url == 'string', 'tab.url should be a string');
-            callback(url);
-        });
-    }
-
-    function generateUUID() {
-        var d = new Date().getTime();
-        var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-            var r = (d + Math.random() * 16) % 16 | 0;
-            d = Math.floor(d / 16);
-            return (c == 'x' ? r : (r & 0x7 | 0x8)).toString(16);
-        });
-        return uuid;
-    };
 });
