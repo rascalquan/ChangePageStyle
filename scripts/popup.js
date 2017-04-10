fa7bf11f85b63b4bff530cbe174d4ec6cf0e7a61
rascalquan <rascalquan@outlook.com>
2017-04-10 23:34:52 +0800

    add template

--- scripts/popup.js
+++ scripts/popup.js
@@ -6,24 +6,23 @@ jQuery(function($) {
                 field: "dataId",
                 hidden: true
             },
-            { field: "selectorField", title: "选择器" },
-            //{ field: "attrKey", title: "属性", editor: attrKeyDropDownEditor, template: ["border", "background", "color"] },
-            { field: "attrKey", title: "属性" },
-            { field: "attrValue", title: "属性值" },
-            { field: "activeRange", title: "生效范围" },
+            { field: "selectorField", title: "Selector", width: "100px" },
+            { field: "attrKey", title: "Attribute", editor: attrKeyAutoCompleteEditor, width: "120px" },
+            { field: "attrValue", title: "AttrValue", editor: attrValueAutoCompleteEditor, width: "120px" },
+            { field: "activeRange", title: "Range", editor: rangeDropdownListEditor, width: "100px" },
             {
-                field: "isAwaysActive",
-                title: "永久生效",
-                //template: "<input type='radio' name='isAwaysActive' value='false' checked='#=(isAwaysActive==false)#'>一次<input type='radio' name='isAwaysActive' value='true' checked='#=(isAwaysActive==true)#'>永久",
-                width: "150px"
+                field: "forever",
+                title: "Forever",
+                //template: "<input type='radio' name='forever' value='false' checked='#=(forever==false)#'>一次<input type='radio' name='forever' value='true' checked='#=(forever==true)#'>永久",
+                width: "80px"
             },
             {
                 command: [
                     { name: "edit", text: "Edit" },
                     { name: "destroy", text: "Delete" }
                 ],
-                title: "操作",
-                width: "200px"
+                title: "Operate",
+                width: "180px"
             }
         ],
         dataSource: {
@@ -33,8 +32,8 @@ jQuery(function($) {
                 destroy: gridDestroy,
                 create: gridCreate
             },
-            // data: [{ dataId: "1", selectorField: "#maindiv", attrKey: "border", attrValue: "1px", activeRange: 0, isAwaysActive: false },
-            //     { dataId: "2", selectorField: "#maindiv", attrKey: "border", attrValue: "1px", activeRange: 0, isAwaysActive: true }
+            // data: [{ dataId: "1", selectorField: "#maindiv", attrKey: "border", attrValue: "1px", activeRange: 0, forever: false },
+            //     { dataId: "2", selectorField: "#maindiv", attrKey: "border", attrValue: "1px", activeRange: 0, forever: true }
             // ],
             schema: {
                 model: {
@@ -59,7 +58,7 @@ jQuery(function($) {
                             type: "string",
                             validation: { required: true }
                         },
-                        isAwaysActive: {
+                        forever: {
                             type: "boolean",
                             defaultValue: false,
                             validation: { required: true }
@@ -79,15 +78,96 @@ jQuery(function($) {
     });
     initFace();
 
-    function attrKeyDropDownEditor(container, options) {
-        $('<input required data-text-field="attrKey" data-value-field="attrKey" data-bind="value:' + options.field + '"/>')
+    function attrKeyAutoCompleteEditor(container, options) {
+        $('<input required  data-bind="value:' + options.field + '"/>')
+            .appendTo(container)
+            .kendoAutoComplete({
+                dataSource: ["align-content",
+                    "align-items", "align-self", "all", "animation", "animation-delay", "animation-direction",
+                    "animation-duration", "animation-fill-mode", "animation-iteration-count", "animation-name",
+                    "animation-play-state", "animation-timing-function", "backface-visibility", "background",
+                    "background-attachment", "background-blend-mode", "background-clip", "background-color",
+                    "background-image", "background-origin", "background-position", "background-repeat",
+                    "background-size", "border", "border-bottom", "border-bottom-color", "border-bottom-left-radius",
+                    "border-bottom-right-radius", "border-bottom-style", "border-bottom-width", "border-collapse",
+                    "border-color", "border-image", "border-image-outset", "border-image-repeat", "border-image-slice",
+                    "border-image-source", "border-image-width", "border-left", "border-left-color", "border-left-style",
+                    "border-left-width", "border-radius", "border-right", "border-right-color", "border-right-style",
+                    "border-right-width", "border-spacing", "border-style", "border-top", "border-top-color",
+                    "border-top-left-radius", "border-top-right-radius", "border-top-style", "border-top-width",
+                    "border-width", "bottom", "box-shadow", "box-sizing", "caption-side", "clear", "clip", "color",
+                    "column-count", "column-fill", "column-gap", "column-rule", "column-rule-color", "column-rule-style",
+                    "column-rule-width", "column-span", "column-width", "columns", "content", "counter-increment",
+                    "counter-reset", "cursor", "direction", "display", "empty-cells", "filter", "flex", "flex-basis",
+                    "flex-direction", "flex-flow", "flex-grow", "flex-shrink", "flex-wrap", "float", "font", "@font-face",
+                    "font-family", "font-size", "font-size-adjust", "font-stretch", "font-style", "font-variant",
+                    "font-weight", "hanging-punctuation", "height", "justify-content", "@keyframes", "left", "letter-spacing",
+                    "line-height", "list-style", "list-style-image", "list-style-position", "list-style-type", "margin",
+                    "margin-bottom", "margin-left", "margin-right", "margin-top", "max-height", "max-width", "@media",
+                    "min-height", "min-width", "nav-down", "nav-index", "nav-left", "nav-right", "nav-up", "opacity",
+                    "order", "outline", "outline-color", "outline-offset", "outline-style", "outline-width", "overflow",
+                    "overflow-x", "overflow-y", "padding", "padding-bottom", "padding-left", "padding-right", "padding-top",
+                    "page-break-after", "page-break-before", "page-break-inside", "perspective", "perspective-origin",
+                    "position", "quotes", "resize", "right", "tab-size", "table-layout", "text-align", "text-align-last",
+                    "text-decoration", "text-decoration-color", "text-decoration-line", "text-decoration-style", "text-indent",
+                    "text-justify", "text-overflow", "text-shadow", "text-transform", "top", "transform", "transform-origin",
+                    "transform-style", "transition", "transition-delay", "transition-duration", "transition-property",
+                    "transition-timing-function", "unicode-bidi", "vertical-align", "visibility", "white-space", "width",
+                    "word-break", "word-spacing", "word-wrap", "z-index"
+                ],
+                pageSize: 20,
+                suggest: true,
+                ignoreCase: true,
+                highlightFirst: false,
+                height: "200px",
+                animation: {
+                    open: {
+                        effects: "fadeIn zoom:in",
+                        duration: 300
+                    },
+                    close: {
+                        effects: "fadeOut zoom:out",
+                        duration: 300
+                    }
+                }
+            });
+    }
+
+    function attrValueAutoCompleteEditor(container, options) {
+        $('<input required  data-bind="value:' + options.field + '"/>')
+            .appendTo(container)
+            .kendoAutoComplete({
+                dataSource: ["color", "border", "background"],
+                suggest: true,
+                ignoreCase: true,
+                highlightFirst: false,
+                height: "100px",
+                animation: {
+                    open: {
+                        effects: "fadeIn zoom:in",
+                        duration: 300
+                    },
+                    close: {
+                        effects: "fadeOut zoom:out",
+                        duration: 300
+                    }
+                }
+            });
+    }
+
+    function rangeDropdownListEditor(container, options) {
+        $('<input required  data-bind="value:' + options.field + '"/>')
             .appendTo(container)
             .kendoDropDownList({
-                autoBind: false,
-                dataSource: {
-                    type: "odata",
-                    transport: {
-                        read: ""
+                dataSource: ["当前", "同级", "同域", "全部"],
+                animation: {
+                    open: {
+                        effects: "fadeIn zoom:in",
+                        duration: 300
+                    },
+                    close: {
+                        effects: "fadeOut zoom:out",
+                        duration: 300
                     }
                 }
             });
@@ -142,7 +222,7 @@ jQuery(function($) {
             attrKey: options.data.attrKey,
             attrValue: options.data.attrValue,
             activeRange: options.data.activeRange,
-            isAwaysActive: Boolean(options.data.isAwaysActive)
+            forever: Boolean(options.data.forever)
         }
         return thisData;
     }
@@ -184,9 +264,9 @@ jQuery(function($) {
     //         console.log("begin save.");
     //     },
     //     datasource: new kendo.data.DataSource({
-    //         data: [{ selectorField: "#maindiv", attrKey: "border", attrValue: "1px", activeRange: 0, isAwaysActive: false },
-    //             { selectorField: "#maindiv", attrKey: "border", attrValue: "1px", activeRange: 0, isAwaysActive: true },
-    //             { selectorField: "#maindiv", attrKey: "border", attrValue: "1px", activeRange: 0, isAwaysActive: false }
+    //         data: [{ selectorField: "#maindiv", attrKey: "border", attrValue: "1px", activeRange: 0, forever: false },
+    //             { selectorField: "#maindiv", attrKey: "border", attrValue: "1px", activeRange: 0, forever: true },
+    //             { selectorField: "#maindiv", attrKey: "border", attrValue: "1px", activeRange: 0, forever: false }
     //         ],
     //         schema: {
     //             model: {
@@ -207,7 +287,7 @@ jQuery(function($) {
     //                         type: "string",
     //                         validation: { required: true }
     //                     },
-    //                     isAwaysActive: {
+    //                     forever: {
     //                         type: "boolean",
     //                         defaultValue: false,
     //                         validation: { required: true }
